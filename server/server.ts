019a0eb2c3cd39b0778967437fb2554b3f219f0f
olzzon <github@olzzon.dk>
2021-03-08 16:26:57 +0100

    fix: use settings webpages to iterate over rooms. As socket.rooms er return some id instead of roomname

--- server/server.ts
+++ server/server.ts
@@ -44,12 +44,12 @@ io.on('connection', (socket) => {
     socket.once('disconnect', () => {})
 
     function updateClientsInRooms() {
-        socket.rooms.forEach((socketRoom) => {
+        settings.webpages.forEach((webpage) => {
             let clientsInRoom = socketClients.filter((client) => {
-                return (client.roomName === socketRoom)
+                return (client.roomName === webpage.id.toString())
             })
             let usersInRoom = clientsInRoom.map((client)=> {return client.userUrlName})
-            io.to(socketRoom).emit('users', JSON.stringify(usersInRoom))
+            io.to(webpage).emit('users', JSON.stringify(usersInRoom))
         })
     }
 

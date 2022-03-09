d308cf0cee3e80d49e189e8edacdc9aa62532fc9
Anders Frederik Jørgensen <afjo@tv2.dk>
2022-03-09 08:47:17 +0100

    refactor: Renamed build to dist for consistency.
    We are using dist as the output folder in our other repos.
    By renaming build to dist we can use the same CI pipelines.

--- webpack.admin.config.js
+++ webpack.admin.config.js
@@ -19,6 +19,6 @@ module.exports = {
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'build/admin'),
+        path: path.resolve(__dirname, 'dist/admin'),
     },
 }
